5ace5ab64e02b329e7a677e183c03fcd2dc92692
Marcin Michalski <marmichalski97@gmail.com>
2018-03-06 18:52:29 +0100

    1.01
    nie dziala przeciazanie operatora +

--- lib/Vector.hpp
+++ lib/Vector.hpp
@@ -8,8 +8,11 @@ public:
 int lenght, l;
 //std::array<double, lenght> vector;
 std::vector<double> vect;
+Vector(){}
 Vector(std::vector<double> v)
 : vect(v){}
 void setVector (int , double);
 double getVector (int );
+Vector operator+(Vector, Vector);
+int sizeOfVector(void);
 };
