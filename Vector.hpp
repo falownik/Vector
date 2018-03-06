a3d62131e008594e75da303153900b17faad7b7c
falownik <36796295+falownik@users.noreply.github.com>
2018-03-06 16:40:58 +0100

    Create Vector.hpp

---
+++ Vector.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+class Vector{
+private:
+
+public:
+int lenght, l;
+std::array<double, lenght>;
+void Vector(std::array<double, l>)
+: std::array<double, lenght>(std::array<double, l>);
+void setVector (int x, double value);
+void getVector (int x);
+}
