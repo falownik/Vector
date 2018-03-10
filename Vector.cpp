ce2985c63bfa43331129745d76eeb83b706fc8a0
Marcin Michalski <marmichalski97@gmail.com>
2018-03-10 20:12:08 +0100

    1.02
    działa przeciążanie +, problemy z estruktorem

--- Vector.cpp
+++ Vector.cpp
@@ -12,17 +12,20 @@ double Vector::getVector(int x){
 int Vector::sizeOfVector(void){
     return vect.size();
 }
-Vector operator+(Vector wektor1, Vector wektor2){
-if (wektor1.sizeOfVector()!=wektor2.sizeOfVector()){
-    std::cout << "blad sumowania wektorow" << std::endl;
-}
-Vector wynik;
-for (int i = 0; i < wektor1.sizeOfVector(); i++){
-    wynik = wektor1 + wektor2;
-}
-return wynik;
-}
+// Definicja przeciążenia <<
+ std::ostream& operator<<(std::ostream& os, Vector& wektor){
+     for (auto b: wektor.vect)
+        os << b;
+    return os;
+ }
 int main(void){
     Vector wektor({1.1,2.1});
-    std::cout << wektor.getVector(1);
+    Vector wektor1({1,2});
+    wektor = wektor + wektor1;
+    //std::cout << wektor.getVector(0);
+    //std::cout << wektor.getVector(1);
+    std::cout << wektor;
+    //for (auto b: wektor.vect)
+    //std::cout << wektor.vect;
+    return 0;
 }
--- lib/Vector.hpp
+++ lib/Vector.hpp
@@ -6,13 +6,25 @@ private:
 
 public:
 int lenght, l;
-//std::array<double, lenght> vector;
 std::vector<double> vect;
 Vector(){}
 Vector(std::vector<double> v)
 : vect(v){}
 void setVector (int , double);
 double getVector (int );
-Vector operator+(Vector, Vector);
 int sizeOfVector(void);
+Vector operator+(Vector wektor2){
+if (this->sizeOfVector()!=(wektor2).sizeOfVector()){
+    std::cout << "blad sumowania wektorow" << std::endl;
+}
+Vector wynik;
+for (int i = 0; i < this->sizeOfVector(); i++){
+    this->setVector(i, this->getVector(i) + (wektor2).getVector(i));
+}
+return wynik;
+}
+//deklaracja przeciążenia operatora <<
+friend std::ostream& operator<<(std::ostream&,Vector &);
+//Destruktor
+~Vector(){}
 };
