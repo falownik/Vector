a88b9de32b618f9cb5ce8716fa2b77f22b8460ee
falownik <36796295+falownik@users.noreply.github.com>
2018-03-27 12:53:00 +0200

    Create Test.hpp

---
+++ lib/Test.hpp
@@ -0,0 +1,19 @@
+//klasa testowa do Vector
+class Test
+{
+private:
+public:
+    Test(){}
+    void testSetVector1Argument (void);
+    void testSetVector2Argument (void);
+    void testOperatorGet(void);
+    void testOperatorRead (void);
+    void testOperatorWrite (std::ostream &);
+    void testOperatorPlus (void);
+    void testOperatorPlusUnary (void);
+    void testOperatorMinus (void);
+    void testOperatorMinusUnary (void);
+    void testOperatorMultiplication (void);
+    void testOperatorMultiplicationUnary (void);
+
+};
